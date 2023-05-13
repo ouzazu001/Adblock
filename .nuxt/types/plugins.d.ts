3087aa1c0f605702acb2d5fa93b9dc67f0f64f19
woratan <woratan@igeargeek.com>
2023-05-13 19:01:47 +0700

    feat: set git

--- .nuxt/types/plugins.d.ts
+++ .nuxt/types/plugins.d.ts
@@ -9,8 +9,7 @@ type NuxtAppInjections =
   InjectionType<typeof import("../components.plugin").default> &
   InjectionType<typeof import("../../node_modules/nuxt/dist/head/runtime/plugins/unhead").default> &
   InjectionType<typeof import("../../node_modules/nuxt/dist/app/plugins/router").default> &
-  InjectionType<typeof import("../../node_modules/nuxt/dist/app/plugins/chunk-reload.client").default> &
-  InjectionType<typeof import("../../node_modules/nuxt/dist/app/plugins/payload.client").default>
+  InjectionType<typeof import("../../node_modules/nuxt/dist/app/plugins/chunk-reload.client").default>
 
 declare module '#app' {
   interface NuxtApp extends NuxtAppInjections { }
