1d219091fdd798b4ec08c59b9148f0bbd1679ff1
woratan <woratan@igeargeek.com>
2023-05-13 18:09:05 +0700

    feat: test nuxt3 in github.io

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/igeargeek/Project/Adblock.github.io/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
